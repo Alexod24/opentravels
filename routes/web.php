65f747b89d870c4364c1648fd148709fac3b71f1
YerikAH <yerik05vh@gmail.com>
2024-04-08 15:59:25 -0500

    :sparkles: Add new views for web page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('web');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/register', function () {
+    return view('register');
+});
+Route::get('/404', function () {
+    return view('404');
 });
